2b035356c2e7503ea210b91f9ee4ba086244e729
houtaruoreki <l.maghlaperidze@gmail.com>
2024-10-16 00:02:15 +0400

    added redirect to swagger

--- chat/urls.py
+++ chat/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.urls import path, include, reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url=reverse_lazy('swagger-ui'), permanent=False)),
     path("auth/", include("user.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 
